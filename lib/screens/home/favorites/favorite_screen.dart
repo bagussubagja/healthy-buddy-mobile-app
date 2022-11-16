58c338a6bbab068426b827b35177a4337744651e
Bagus Subagja <bagussubagja17@gmail.com>
2022-11-16 14:42:36 +0700

    Add Favorite and Wishlist Screen

--- lib/screens/home/favorites/favorite_screen.dart
+++ lib/screens/home/favorites/favorite_screen.dart
@@ -1,14 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:healthy_buddy_mobile_app/screens/widgets/margin_height.dart';
+import 'package:healthy_buddy_mobile_app/shared/theme.dart';
+import 'package:sizer/sizer.dart';
 
-class FavoriteScreen extends StatelessWidget {
+import '../../widgets/margin_width.dart';
+
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
   @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  int _currentIndex = 0;
+  List<bool> _selectedToogle = [true, false, false, false];
+  List<String> _categoryLabel = ["All", "Foodies", "MyDoc", "Sport"];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Ini Favorite Screen'),
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Padding(
+          padding: defaultPadding,
+          child: ListView(
+            children: [
+              _headerSection(),
+              _toogleButton(),
+              MarginHeight(height: 2.h),
+              _itemList()
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _headerSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Favorite',
+          style: titleStyle.copyWith(color: greenColor),
+        ),
+        Text(
+          'Healthy Buddy - Always by your side!',
+          style: regularStyle.copyWith(color: greyTextColor),
+        ),
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 50.h,
+          child: SvgPicture.asset('assets/svg/favorite.svg'),
+        ),
+      ],
+    );
+  }
+
+  Widget _toogleButton() {
+    return Center(
+      child: SizedBox(
+        height: 30,
+        child: ListView.separated(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    for (int i = 0; i < _selectedToogle.length; i++) {
+                      if (i == index) {
+                        _selectedToogle[i] = true;
+                      } else {
+                        _selectedToogle[i] = false;
+                      }
+                    }
+                  });
+                  _currentIndex = index;
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(2),
+                  height: 30,
+                  width: 80,
+                  decoration: BoxDecoration(
+                      color: _selectedToogle[index]
+                          ? greenColor.withOpacity(0.2)
+                          : greyColor,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                          color: _selectedToogle[index]
+                              ? greenDarkerColor
+                              : greyTextColor)),
+                  child: Text(
+                    _categoryLabel[index],
+                    style: regularStyle.copyWith(
+                        color: _selectedToogle[index]
+                            ? greenDarkerColor
+                            : greyTextColor,
+                        fontSize: 14),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return MarginWidth(width: 3.w);
+            },
+            itemCount: 4),
+      ),
+    );
+  }
+
+  Widget _itemList() {
+    return ListView.separated(
+      separatorBuilder: (context, index) {
+        return MarginHeight(height: 2.h);
+      },
+      shrinkWrap: true,
+      primary: false,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Container(
+          width: double.infinity,
+          height: 15.h,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(12)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 13.h,
+                width: 13.h,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/images/discount-offers.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 60.w,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Sandwich',
+                      style: titleStyle.copyWith(fontSize: 12.sp),
+                    ),
+                    Text(
+                      'Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet. Lorem Ipsum Dolor Sit Amet.',
+                      style: regularStyle.copyWith(
+                          color: greyTextColor, fontSize: 10.sp),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
